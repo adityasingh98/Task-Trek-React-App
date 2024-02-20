0351f7ae565bac3d66b763e1766303f6b04fd8c7
Aditya Singh <as873311@gmail.com>
2024-02-20 20:46:44 +0530

    First Commit

---
+++ src/components/TaskColumn.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./TaskColumn.css";
+import TaskCard from "./TaskCard";
+
+const TaskColumn = ({ name, icon, tasks, status, handleDelete }) => {
+  return (
+    <section className="task_column">
+      <h2 className="task_column_heading">
+        <img className="task_column_icon" src={icon} alt="" />
+        {name}
+      </h2>
+
+      {tasks.map(
+        (task, index) =>
+          task.status === status && (
+            <TaskCard
+              key={index}
+              title={task.task}
+              tags={task.tags}
+              handleDelete={handleDelete}
+              index={index}
+            />
+          )
+      )}
+    </section>
+  );
+};
+
+export default TaskColumn;
